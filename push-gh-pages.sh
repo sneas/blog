a826ecc7eb9d97c5a6e6666c9e46a6e306ad4a16
Dima Snisarenko <snisarenkodima@gmail.com>
2018-10-06 15:42:04 +0200

    Add git email and username

--- push-gh-pages.sh
+++ push-gh-pages.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git config user.email "snisarenkodima@gmail.com"
+git config user.name "Dima Snisarenko (CircleCI)"
 git commit -am "Save uncommited changes (WIP)"
 git branch --delete --force gh-pages
 git checkout --orphan gh-pages
