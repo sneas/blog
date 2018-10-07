5eeab557e6af11fe132e2bb6a37c1fd32b01a9f1
Dima Snisarenko <snisarenkodima@gmail.com>
2018-10-07 08:44:36 +0200

    Update push-gh-pages.sh to stash previously applied changes and condintionally set up github name and email

--- .circleci/push-gh-pages.sh
+++ .circleci/push-gh-pages.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-git config user.email $GH_EMAIL
-git config user.name $GH_NAME
-git commit -am "Save uncommited changes (WIP)"
+[ ! -z "$GH_NAME" ] && git config user.email $GH_NAME
+[ ! -z "$GH_EMAIL" ] && git config user.email $GH_EMAIL
+
+MAIN_BRANCH=$(git symbolic-ref --short HEAD)
+
+git stash
 git branch --delete --force gh-pages
 git checkout --orphan gh-pages
 git add -f dist
 git commit -m "Rebuild GitHub pages [ci skip]"
-git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages && git checkout -
+git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages
+git checkout $MAIN_BRANCH
+git stash apply
