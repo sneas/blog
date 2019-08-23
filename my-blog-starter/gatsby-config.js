a2acfd0e4ce74b7639e3ec7e40c106570bc84c38
Dima Snisarenko <snisarenkodima@gmail.com>
2019-08-23 17:12:55 +0200

    Add gatsby version of the blog into temp folder

---
+++ my-blog-starter/gatsby-config.js
@@ -0,0 +1,87 @@
+module.exports = {
+  pathPrefix: '/blog/my-blog-starter/public',
+  siteMetadata: {
+    title: `Dev Blog`,
+    author: `Dmitry Snisarenko`,
+    description: `A bit of insight into software design.`,
+    siteUrl: `https://sneas.github.io/blog`,
+    social: {
+      twitter: `_sneas`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-sitemap`
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              inlineCodeMarker: '÷',
+            },
+          },
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-135464443-1`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Dmitry Snisarenko's Dev blog`,
+        short_name: `Deb blog`,
+        start_url: `/blog`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/icon.jpg`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
